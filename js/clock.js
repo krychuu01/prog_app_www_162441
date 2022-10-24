b61b712bbe72bb247dde9458bee222d02a65a5ec
GKrych <hazzidy125@gmail.com>
2022-10-24 22:04:51 +0200

    added clock with custom font to all pages

---
+++ js/clock.js
@@ -0,0 +1,29 @@
+setInterval(displayClock, 1000);
+
+function displayClock() {
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+ 
+    if (hours > 12) {
+        hours -= 12;
+        am_pm = "PM";
+    }
+    if (hours == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+ 
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+ 
+    let currentTime = hours + ":"
+            + minutes + ":" + seconds + " " + am_pm;
+ 
+    document.getElementById("clock")
+            .innerHTML = currentTime;
+}
+
+displayClock();
